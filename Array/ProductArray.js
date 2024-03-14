393af0a2343cbd9b609451cb7de6ba133effce3c
Edison Augusthy <edisonaugusthy@Edisons-MacBook-Pro.local>
2024-03-14 23:07:41 +0530

    added solution

---
+++ Array/ProductArray.js
@@ -0,0 +1,15 @@
+// Input
+//const input = [1, 2, 3, 4, 5];
+// Output: buildProductArray(input)
+// [2*3*4*5, 1*3*4*5, 1*2*4*5, 1*2*3*5, 1*2*3*4]
+//[120, 60, 40, 30, 24];
+
+/**
+ * @param {List [int]} input
+ * @return {List [int]}
+ */
+const buildProductArray = (input) => {
+  // Your solution here
+};
+
+buildProductArray([1, 2, 3, 4, 5]);
